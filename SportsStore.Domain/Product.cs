ba756a25527959e8c05627ae9c487aa18ae7f56f
Huynh The Nghia <0306241130@caothang.edu.vn>
2026-09-07 10:52:02 +0700

    lap 9

--- SportsStore.Domain/Product.cs
+++ SportsStore.Domain/Product.cs
@@ -5,19 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using System.ComponentModel.DataAnnotations;
 namespace SportsStore.Domain
 {
     public class Product
     {
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
         public  string? Name { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập mô tả")]
         public  string? Description { get; set; }
 
+        [Required]
+        [Range(0.01,double.MaxValue,ErrorMessage = "Vui lòng nhập giá trị dương")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập danh mục")]
         public  string? Category { get; set; }
 
         public string? ImgUrl { get; set; }
